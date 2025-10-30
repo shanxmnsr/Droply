cc56328faa1d725fa57b6a8a77102fad86952d26
Shania Mansoori <shaniabca2024@gmail.com>
2025-10-30 14:14:29 +0530

    Initial Setup Complete

--- lib/imagekit.ts
+++ lib/imagekit.ts
@@ -1,7 +1,9 @@
 import ImageKit from "imagekit";
 
-export const imagekit = new ImageKit({
-  publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
+const imagekit = new ImageKit({
+  publicKey: process.env.IMAGEKIT_PUBLIC_KEY!,
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
   urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!,
 });
+
+export default imagekit;
